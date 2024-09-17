e80cd0544792529c4354bc7a2ae16d5338c4569e
GenaroMauleon <168040339+GenaroMauelon@users.noreply.github.com>
2024-09-17 00:08:44 -0300

    Add files via upload

---
+++ tp.js
@@ -0,0 +1,18 @@
+//let informacion = fetch ("https://fakestoreapi.com/products");
+
+//console.log(informacion); 
+
+let contenedor = document.getElementById("contenedor");
+
+fetch("https://freetestapi.com/api/v1/movies")
+    .then(resp => resp.json())
+    .then(data => {
+        data.forEach(prod => {
+            contenedor.innerHTML += `<h1> Titulo: ${prod.title} </h1>
+                                     <h3> Año: ${prod.year} </h3>
+                                     <h3> Genero: ${prod.genre} </h3>
+                                     <h3> Rating: ${prod.rating} </h3>
+                                     <h3> Director: ${prod.director} </h3>`
+        })
+    })
+    .catch(err => console.log(err))
